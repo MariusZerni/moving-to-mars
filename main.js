2d4b1ba20d3156de95d831b49176cd11362b0228
Marius Zerni <marius_zerni@yahoo.com>
2020-02-21 23:15:09 +0000

    invaders disappear

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 
 
 
-const width = 20
-const gridCellCount = width * width
-const cells = []
-let player = 389
-let invaders = [44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 134, 135]
-// const invaders = [44, 45, 64, 65]
-
-const rightColumIds = [19, 39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 239, 259, 279, 299, 319, 339, 359, 379, 399]
-let invadersDirection = 'right'
-
-
 function projectOne() {
+
+  const width = 20
+  const gridCellCount = width * width
+  const cells = []
+  let player = 389
+  let laser = 0
+  const invaders = [44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135]
+
+
+
+  let invadersDirection = 'right'
+
+
+
   const grid = document.querySelector('.grid')
 
   createGrid(grid)
@@ -28,134 +31,220 @@ function projectOne() {
 
   controlPlayer()
 
-  // moveInvaders()
+  moveInvaders()
 
 
-}
+  function createGrid(grid) {
 
-window.addEventListener('DOMContentLoaded', projectOne)
+    for (let i = 0; i < gridCellCount; i++) {
 
+      const cell = document.createElement('div')
+      cell.innerHTML = i
 
-function createGrid(grid) {
+      cell.classList.add('cell')
 
-  for (let i = 0; i < gridCellCount; i++) {
-
-    const cell = document.createElement('div')
-    cell.innerHTML = i
-
-    cell.classList.add('cell')
-
-    grid.appendChild(cell)
-    cells.push(cell)
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
   }
-}
 
-function moveInvaders() {
+  function moveInvaders() {
 
-  const intervalId = setInterval(() => {
-    if (invadersDirection === 'right') {
+    const intervalId = setInterval(() => {
+      if (invadersDirection === 'right') {
 
-      for (let i = 0; i < invaders.length; i++) {
-        const invader = invaders[i]
-        cells[invader].classList.remove('invaders')
-      }
-
-      let lastColumn = false
-
-      for (let i = 0; i < invaders.length; i++) {
-        const invader = invaders[i]
-        if ((invader + 1) % 20 === 0) {
-          lastColumn = true
+        for (let i = 0; i < invaders.length; i++) {
+          const invader = invaders[i]
+          cells[invader].classList.remove('invaders')
         }
-      }
 
-      for (let i = 0; i < invaders.length; i++) {
+        let lastColumn = false
 
-        let invader = invaders[i]
+        for (let i = 0; i < invaders.length; i++) {
+          const invader = invaders[i]
+          if ((invader + 1) % 20 === 0) {
+            lastColumn = true
+          }
+        }
+
+        for (let i = 0; i < invaders.length; i++) {
+
+          let invader = invaders[i]
+
+          if (lastColumn) {
+            invader = invader + 20
+          } else {
+            invader = invader + 1
+          }
+
+          invaders[i] = invader
+          cells[invader].classList.add('invaders')
+        }
 
         if (lastColumn) {
-          invader = invader + 20
-        } else {
-          invader = invader + 1
+          invadersDirection = 'left'
         }
 
-        invaders[i] = invader
-        cells[invader].classList.add('invaders')
-      }
+      } else if (invadersDirection === 'left') {
 
-      if (lastColumn) {
-        invadersDirection = 'left'
-      }
+        for (let i = 0; i < invaders.length; i++) {
+          const invader = invaders[i]
 
-    } else if (invadersDirection === 'left') {
-
-      for (let i = 0; i < invaders.length; i++) {
-        const invader = invaders[i]
-
-        cells[invader].classList.remove('invaders')
-      }
-
-      let firstColumn = false
-      for (let i = 0; i < invaders.length; i++) {
-        const invader = invaders[i]
-        if ((invader) % 20 === 0) {
-          firstColumn = true
+          cells[invader].classList.remove('invaders')
         }
-      }
 
-      for (let i = 0; i < invaders.length; i++) {
+        let firstColumn = false
+        for (let i = 0; i < invaders.length; i++) {
+          const invader = invaders[i]
+          if ((invader) % 20 === 0) {
+            firstColumn = true
+          }
+        }
 
-        let invader = invaders[i]
+        for (let i = 0; i < invaders.length; i++) {
+
+          let invader = invaders[i]
+
+          if (firstColumn) {
+            invader = invader + 20
+          } else {
+            invader = invader - 1
+          }
+
+          invaders[i] = invader
+          cells[invader].classList.add('invaders')
+        }
 
         if (firstColumn) {
-          invader = invader + 20
-        } else {
-          invader = invader - 1
+          invadersDirection = 'right'
         }
-
-        invaders[i] = invader
-        cells[invader].classList.add('invaders')
       }
 
-      if (firstColumn) {
-        invadersDirection = 'right'
-      }
-    }
+
+      invaders.forEach((invader) => {
+        if (invader >= 380) {
+          clearInterval(intervalId)
+        }
+      })
+
+    }, 2000)
+  }
 
 
-    invaders.forEach((invader) => {
-      if (invader >= 380) {
-        clearInterval(intervalId)
+  function controlPlayer() {
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'ArrowRight') {
+        if (player === cells.length - 1) {
+          return
+        }
+        cells[player].classList.remove('player')
+        player += 1
+        cells[player].classList.add('player')
+      } else if (event.key === 'ArrowLeft') {
+        if (player === cells.length - 20) {
+          return
+        }
+        cells[player].classList.remove('player')
+        player -= 1
+        cells[player].classList.add('player')
       }
+
     })
 
-  }, 500)
-}
+    let firstRun = true
 
-function controlPlayer() {
+    setInterval(() => {
 
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowRight') {
-      if (player === cells.length - 1) {
-        return
+
+      if (firstRun) {
+        laser = (player - 20)
+        firstRun = false
+      } else if (!(laser / 20 === 0)) {
+        cells[laser].classList.remove('laser')
+        if (laser - 20 >= 0) {
+          laser -= 20
+        } else {
+          laser = player - 20
+        }
+
+      } else if (laser / 20 === 0) {
+        console.log('firstRow')
+        cells[laser].classList.remove('laser')
+        laser = player - 20
       }
-      cells[player].classList.remove('player')
-      player += 1
-      cells[player].classList.add('player')
-    } else if (event.key === 'ArrowLeft') {
-      if (player === cells.length - 20) {
-        return
+
+      cells[laser].classList.add('laser')
+      makeInvadersDisappear()
+
+    }, 600)
+
+
+
+
+  }
+
+
+  function makeInvadersDisappear() {
+
+    let foundInvader = -1
+    invaders.forEach((invader) => {
+      if (invader === laser) {
+        cells[invader].classList.remove('invaders')
+        foundInvader = invader
       }
-      cells[player].classList.remove('player')
-      player -= 1
-      cells[player].classList.add('player')
+    })
+    if (foundInvader !== -1) {
+      const index = invaders.indexOf(foundInvader)
+      invaders.splice(index,1)
+      console.log(foundInvader + ' ' + invaders)
     }
 
-  })
+  }
+  makeInvadersDisappear()
+
+
+
+
+  function generateBombs() {
+    const secondIntervalId = setInterval(() => {
+      const invadersDropBombs = invaders.slice(-12)
+      let randomInvader = invadersDropBombs[Math.floor(Math.random() * (invadersDropBombs.length))]
+      cells[randomInvader].classList.add('bombs')
+      console.log(randomInvader)
+      setInterval(() => {
+
+        if (randomInvader < 399) {
+          cells[randomInvader].classList.remove('bombs')
+
+
+          if (randomInvader + 20 < 399) {
+            randomInvader += 20
+            cells[randomInvader].classList.add('bombs')
+          } else {
+            cells[randomInvader].classList.remove('bombs')
+          }
+        }
+      }, 500)
+
+    }, 2000)
+  }
+  generateBombs()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-function rightColumnStop() {
 
-
-
-}
+window.addEventListener('DOMContentLoaded', projectOne)
